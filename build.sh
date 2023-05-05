b53ce395f2f964399b7a54fb6a94d93366b8f6bf
PandaNinjas <admin@malwarefight.gq>
2023-05-04 19:43:58 -0700

    chore(ci): improvements to build.sh (#8)
    Co-authored-by: Ryan Cao <70191398+ryanccn@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
 
-source ".venv/bin/activate"
+[ ! -f .venv/bin/activate ] && python -m venv .venv
+
+. ".venv/bin/activate"
 
 echo "Building frontend..."
 
-cd "osmosis/frontend"
+cd "osmosis/frontend" || exit 
+
+[ ! -e node_modules ] && yarn install
+
 yarn build
 
 echo "Building Python project..."
 
-cd "../.."
+cd "../.." || exit
+
+python -m pip freeze | grep -E ^build
+[ $? -eq 1 ] && python -m pip install build
+
 python -m build
